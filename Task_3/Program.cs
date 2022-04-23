705f6772f8a8abc425125bb2b963218be655f030
Fedor <fedor_prg@rambler.ru>
2022-04-24 09:55:59 +1000

    Created Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[length - 1]}.");
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
