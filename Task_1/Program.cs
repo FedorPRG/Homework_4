02a47f36049b6872c3f624558130d6e8eeed644a
Fedor <fedor_prg@rambler.ru>
2022-04-24 09:23:03 +1000

    Created Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int c=1;
+for (int i = 1; i <= b; i++)
+{
+    c=c*a;
+}
+Console.WriteLine($"{a} в степени {b} равно {c}");
+
