36eb52ec32320716715082fc58b1b2f971389f55
Fedor <fedor_prg@rambler.ru>
2022-04-24 10:21:01 +1000

    Created Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int Summa(int a)
+{
+    int s = 0;
+    int b = a;
+    for (int i = 10; i < a * 10; i = i * 10)
+    {
+        s = s + b % 10;
+        b = b / 10;
+    }
+    return s;
+}
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int sum = Summa(a);
+Console.WriteLine($"{a} -> {sum}");
